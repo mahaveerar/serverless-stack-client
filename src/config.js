5f8fceaf554781822a944a3a30b13cc57d74a42c
Maha <mahaveerar@gmail.com>
2020-03-04 16:03:04 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'ap-south-1',
+    BUCKET: 'uploads-from-notes-app'
+  },
+  apiGateway: {
+    REGION: 'ap-south-1',
+    URL: 'https://llijsoadne.execute-api.ap-south-1.amazonaws.com/dev'
+  },
+  cognito: {
+    REGION: 'ap-south-1',
+    USER_POOL_ID: 'ap-south-1_fGKovQ3mG',
+    APP_CLIENT_ID: '2jp3rnra79vft2mspl404of7eu',
+    IDENTITY_POOL_ID: 'ap-south-1:e58f4097-d7ad-4212-8f61-91d52061410b'
+  }
+};
